5e0dfc31ad5f0f470b644399cd943db33dde4fe1
wushengzhu <ws_zhu@foxmail.com>
2024-08-04 23:50:32 +0800

    feat: add Layout

---
+++ src/views/Layout/components/LayoutAside.vue
@@ -0,0 +1,43 @@
+<template>
+    <el-aside :class="'layout-aside-' + 'default' + ' ' + 'shrink'">
+        <Logo />
+        <!-- <MenuVerticalChildren /> -->
+        <MenuVertical />
+    </el-aside>
+</template>
+
+<script setup lang="ts">
+import Logo from '@/views/Layout/components/navBar/logo.vue'
+import MenuVertical from '@/views/Layout/components/menus/MenuVertical.vue'
+// import MenuVerticalChildren from '@/views/Layout/components/menus/MenuVerticalChildren.vue'
+
+const menuWidth = '200px'
+</script>
+
+<style scoped lang="scss">
+.layout-aside-Default {
+    background: var(--ba-bg-color-overlay);
+    margin: 16px 0 16px 16px;
+    height: calc(100vh - 32px);
+    box-shadow: var(--el-box-shadow-light);
+    border-radius: var(--el-border-radius-base);
+    overflow: hidden;
+    transition: width 0.3s ease;
+    width: v-bind(menuWidth);
+}
+.layout-aside-Classic,
+.layout-aside-Double {
+    background: var(--ba-bg-color-overlay);
+    margin: 0;
+    height: 100vh;
+    overflow: hidden;
+    transition: width 0.3s ease;
+    width: v-bind(menuWidth);
+}
+.shrink {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 9999999;
+}
+</style>
---
+++ src/views/Layout/components/LayoutHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <el-header class="layout-header">
+        <component :is="DefaultNavBar"></component>
+    </el-header>
+</template>
+<script setup lang="ts">
+import DefaultNavBar from '@/views/Layout/components/navBar/DefaultNav.vue'
+
+</script>
+
+<style scoped lang="scss">
+.layout-header {
+    height: auto;
+    padding: 0;
+}
+</style>
